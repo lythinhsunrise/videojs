86b5255f88ff36cecec06763e69208a54d203e57
giathinh.ly <giathinh.ly@urekamedia.vn>
2022-02-08 09:40:02 +0700

    chinh lai chi goi 1 video

--- playerima.js
+++ playerima.js
@@ -43,31 +43,29 @@ player.on('timeupdate', function() {
 var videos = document.getElementsByTagName("video");
 
 function playPauseVideo() {
-  let videos = document.querySelectorAll("video");
-  videos.forEach((video) => {
-      video.muted = true;
-      let playPromise = video.play();
-      if (playPromise !== undefined) {
-          playPromise.then((_) => {
-              let observer = new IntersectionObserver(
-                  (entries) => {
-                      entries.forEach((entry) => {
-                          if (
-                              entry.intersectionRatio !== 1 &&
-                              !video.paused
-                          ) {
-                              video.pause();
-                          } else if (video.paused) {
-                              video.play();
-                          }
-                      });
-                  },
-                  { threshold: 0.2 }
-              );
-              observer.observe(video);
-          });
-      }
-  });
+  let video = document.querySelector("#content_video_html5_api");
+  video.muted = true;
+  let playPromise = video.play();
+  if (playPromise !== undefined) {
+      playPromise.then((_) => {
+          let observer = new IntersectionObserver(
+              (entries) => {
+                  entries.forEach((entry) => {
+                      if (
+                          entry.intersectionRatio !== 1 &&
+                          !video.paused
+                      ) {
+                          video.pause();
+                      } else if (video.paused) {
+                          video.play();
+                      }
+                  });
+              },
+              { threshold: 0.2 }
+          );
+          observer.observe(video);
+      });
+  }
 }
 
 playPauseVideo();
